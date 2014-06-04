2ce4aa56e0c6c759d569d38f3636ed5daa792287
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 16:28:01 -0700

    Implement makeQueue factory. Implement size method.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,14 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var instance = { storage: {}, _size: 0, index: 0 };
+
+  return _.extend(instance, queueMethods);
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function () { return this._size; }
+};
 
 
 
