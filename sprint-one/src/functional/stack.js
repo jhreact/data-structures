8bc416846790890f7ef40ed7d3b7e4d34e9d36b8
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 14:37:17 -0700

    Implement size, and push methods.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,24 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
+  // need a size data structure
+  var size = 0;
 
   // Implement the methods below
   someInstance.push = function(value){
+    // add new items to that stack
+    //    increment the size property after adding
+    //    add value using a number key
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
   };
 
   someInstance.size = function(){
+    // return size of stack
+    return size;
   };
 
   return someInstance;
