ca2a71aad5a5347ad593ffaa747da357256c1996
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 21:25:38 -0700

    Implement size method.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,12 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this._size = 0;
+  this._storage = {};
+};
+
+Stack.prototype.size = function () {
+  return this._size;
 };
 
 
