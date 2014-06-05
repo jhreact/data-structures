70a7c4b5c9146cb6f837ddf4649e151b59ea6411
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 21:38:46 -0700

    Report a size of 2 after adding 2 items.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -2,9 +2,14 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   this._size = 0;
+  this._storage = {};
+  this._index = 0;
 };
 
 Queue.prototype.size = function() {
   return this._size;
 }
 
+Queue.prototype.enqueue = function(value) {
+  this._storage[this._index + this._size++] = value;
+};
