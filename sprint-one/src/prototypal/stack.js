bf665a9fa8d20b69411e71f3453e166c537bb68c
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 20:55:32 -0700

    Implement size() and push() methods.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,16 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack._size = 0;
+  newStack._storage = {};
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+};
 
-
+stackMethods.push = function(value) {
+  this._storage[this._size++] = value;
+};
+stackMethods.size = function() {return this._size;}
