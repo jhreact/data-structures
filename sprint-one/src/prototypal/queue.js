1b81d24e97c03a5ade82460f59ed44e8ef8f4b20
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-04 21:02:25 -0700

    Implement size method. Create prototypal instance.

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,15 @@
 var makeQueue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue._size = 0;
+  newQueue._storage = {};
+  newQueue._index = 0;
+  return newQueue;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() { return this._size;}
+  };
 
 
