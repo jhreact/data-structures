66b2c57ad75cc7705c71bc7122460f42815d8aff
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-06 16:43:59 -0700

    Should add items to a set.

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,9 +7,19 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  // initialize storage if undefined, {}
+  // if storage is an object use item as a key for lookup
+  if (!this._storage) {
+    this._storage = {};
+  }
+  this._storage[item] = true;
 };
 
 setPrototype.contains = function(item){
+  if (!this._storage) {
+    return false;
+  }
+  return !!this._storage[item];
 };
 
 setPrototype.remove = function(item){
