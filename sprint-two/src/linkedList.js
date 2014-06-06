77747e7a8b8dfe4bc84a9629169555554ea58431
Eric Schmitt <ericschmitt1@gmail.com>
2014-06-06 15:01:12 -0700

    Remove head from list when removeHead is called.

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,10 +4,25 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
-    list.tail = makeNode(value);
+    var node = makeNode(value);
+    if (!list.tail) {
+      list.tail = node;
+    } else {
+      list.tail.next = node;
+      list.tail = node;
+    }
+    if (!list.head) {
+      list.head = list.tail;
+    }
   };
 
   list.removeHead = function(){
+    // if head exists
+    //    get the value of next property from head
+    //    update the list.head value
+    if (list.head) {
+      list.head = list.head.next;
+    }
   };
 
   list.contains = function(target){
