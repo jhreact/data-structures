080e9ff05ca65f5c9caf4370f81c84c18a342951
taojonesin <taojonesin (at) nowhere.com>
2014-06-07 20:22:35 -0700

    BinarySearchTree inserts values at the correct location in the tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,19 +1,34 @@
 var makeBinarySearchTree = function(value){
   var tree = Object.create(makeBinarySearchTree.methods);
+  tree.left = undefined;
+  tree.right = undefined;
+  tree.value = value;
   return tree;
 };
 
 makeBinarySearchTree.methods = {};
 
-makeBinarySearchTree.methods.insert = function () {
+makeBinarySearchTree.methods.insert = function(value) {
+  if (value < this.value) {
+    if (! this.left) {
+      this.left = makeBinarySearchTree(value);
+    } else {
+      this.left.insert(value);
+    }
+  } else {
+    if (! this.right) {
+      this.right = makeBinarySearchTree(value);
+    } else {
+      this.right.insert(value);
+    }
+  }
+};
+
+makeBinarySearchTree.methods.contains = function(value) {
 
 };
 
-makeBinarySearchTree.methods.contains = function () {
-
-};
-
-makeBinarySearchTree.methods.depthFirstLog = function () {
+makeBinarySearchTree.methods.depthFirstLog = function(callback) {
 
 };
 
